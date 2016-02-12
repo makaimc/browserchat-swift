722d2dc852ab25efd84e56a86f2380a5f15cf4e9
Matt Makai <matthew.makai@gmail.com>
2016-02-12 11:52:19 -0800

    working towards integrated chat and browsing

--- BrowserChat/ViewController.swift
+++ BrowserChat/ViewController.swift
@@ -10,25 +10,79 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
-  @IBOutlet var urlBar: UITextField!
-  @IBOutlet var webBrowser: UIWebView!
 
+  @IBOutlet var chatWindow: UITextView!
+  @IBOutlet var chatControlsStackView: UIStackView!
+  @IBOutlet var sendChatText: UITextField!
+  @IBOutlet var sendButton: UIButton!
+  
+  @IBOutlet var browserControlsStackView: UIStackView!
+  @IBOutlet var urlText: UITextField!
+  @IBOutlet var goButton: UIButton!
+  @IBOutlet var webBrowser: UIWebView!
+  
+  let purpleBox = UIView()
+  let goldBox = UIView()
+  
+
+  let mainStackView = UIStackView()
+  let chatStackView = UIStackView()
+  let browserStackView = UIStackView()
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    let url = NSURL(string: "https://www.twilio.com/blog/2015/12/designing-chat-applications-for-ios-using-swift.html")!
+    let url = NSURL(string: "https://www.twilio.com/blog")!
     webBrowser.loadRequest(NSURLRequest(URL: url))
+    view.addSubview(mainStackView)
+    
+    purpleBox.backgroundColor = UIColor.purpleColor()
+    goldBox.backgroundColor = UIColor.yellowColor()
+
+    /*
+    mainStackView.addArrangedSubview(purpleBox)
+    mainStackView.addArrangedSubview(goldBox)
+    */
+    
+    mainStackView.addArrangedSubview(chatStackView)
+    mainStackView.addArrangedSubview(browserStackView)
+    
+    
+    chatStackView.addArrangedSubview(chatWindow)
+    chatStackView.addArrangedSubview(chatControlsStackView)
+    
+    browserStackView.addArrangedSubview(browserControlsStackView)
+    browserStackView.addArrangedSubview(webBrowser)
+    
+    mainStackView.axis = UILayoutConstraintAxis.Vertical
+    mainStackView.distribution = UIStackViewDistribution.FillEqually
+    chatStackView.axis = UILayoutConstraintAxis.Vertical
+    browserStackView.axis = UILayoutConstraintAxis.Vertical
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-  @IBAction func goURL(sender: AnyObject) {
-    let url = NSURL(string: self.urlBar.text!)!
-    webBrowser.loadRequest(NSURLRequest(URL: url))
+  
+  override func viewDidLayoutSubviews() {
+    let top = topLayoutGuide.length
+    let bottom = bottomLayoutGuide.length
+    mainStackView.frame = CGRect(x: 0, y: top, width: view.frame.width, height: view.frame.height - top - bottom).insetBy(dx: 0, dy: 0)
+    
+    dispatch_async(dispatch_get_main_queue())
+      {
+        if self.view.frame.height > self.view.frame.width
+        {
+          self.mainStackView.axis = UILayoutConstraintAxis.Vertical
+        }
+        else
+        {
+          self.mainStackView.axis = UILayoutConstraintAxis.Horizontal
+        }
+    }
   }
+
 
   func textFieldShouldReturn(textField: UITextField) -> Bool {
     let url = NSURL(string: textField.text!)!
